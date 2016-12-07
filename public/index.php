cadbfcae391f225c6048b94a6af13d15d089edf0
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-12-07 18:53:44 +0200

    Resovle php7 production error

--- public/index.php
+++ public/index.php
@@ -1,24 +1,22 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../core/bootstrap/app.php';
 
-echo phpinfo();
-//require_once __DIR__ . '/../vendor/autoload.php';
-//require_once __DIR__ . '/../core/bootstrap/app.php';
-//
-//use Core\Request;
-//use Core\Router;
-//use Core\Session;
-//
-//
-//Router::load([
-//    'guest',
-//    'auth',
-//]);
-//
-//echo Router::direct(Request::uri(), Request::method());
-//
-//// By this point all backend code is executed.
-//// We use the below session instead of the server referer as we do not trust the users.
-//if (Request::methodIsGet())
-//{
-//    Session::put(PREVIOUS_URL, Request::absoluteUri());
-//}
+use Core\Request;
+use Core\Router;
+use Core\Session;
+
+
+Router::load([
+    'guest',
+    'auth',
+]);
+
+echo Router::direct(Request::uri(), Request::method());
+
+// By this point all backend code is executed.
+// We use the below session instead of the server referer as we do not trust the users.
+if (Request::methodIsGet())
+{
+    Session::put(PREVIOUS_URL, Request::absoluteUri());
+}
