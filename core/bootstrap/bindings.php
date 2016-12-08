810b00959956783824721438c48abf19905351c0
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-12-08 14:16:48 +0200

    Test that problem is with binding the DB

--- core/bootstrap/bindings.php
+++ core/bootstrap/bindings.php
@@ -8,7 +8,7 @@ use Core\flash\Flash;
 
 App::bind('config.app', require __DIR__ . '/../../config/app.php');
 
-App::bind('config.database', require __DIR__ . '/../../config/database.php');
+//App::bind('config.database', require __DIR__ . '/../../config/database.php'); //ToDo: Create the DB
 
 App::bind('flash', Flash::class);
 
