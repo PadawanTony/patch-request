a8da82f2ab4ac9914928c59f739b76df3eae8504
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-12-08 15:02:08 +0200

    Original setup

--- public/index.php
+++ public/index.php
@@ -5,21 +5,18 @@ require_once __DIR__ . '/../core/bootstrap/app.php';
 use Core\Request;
 use Core\Router;
 use Core\Session;
-//
-//
-//Router::load([
-//    'guest',
-//    'auth',
-//]);
-//
-//echo Router::direct(Request::uri(), Request::method());
-//
-//// By this point all backend code is executed.
-//// We use the below session instead of the server referer as we do not trust the users.
-//if (Request::methodIsGet())
-//{
-//    Session::put(PREVIOUS_URL, Request::absoluteUri());
-//}
 
-$apache_errorlog = file_get_contents('/var/log/apache2/error.log');
-echo exec('cat /var/log/apache2/error.log');
+
+Router::load([
+    'guest',
+    'auth',
+]);
+
+echo Router::direct(Request::uri(), Request::method());
+
+// By this point all backend code is executed.
+// We use the below session instead of the server referer as we do not trust the users.
+if (Request::methodIsGet())
+{
+    Session::put(PREVIOUS_URL, Request::absoluteUri());
+}
